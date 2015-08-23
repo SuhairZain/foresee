a7d19d24711210832ba74647e9eb4f762df48587
Suhair Zain <suhairzain11@gmail.com>
2015-08-23 12:01:23 +0530

    Made a modification to Py file

--- Hardware/pi.py
+++ Hardware/pi.py
@@ -19,12 +19,13 @@ while True:
     t[i]=tem[0]
     t[i]=int(t[i])
     print 'Loop Point 2'
-    if (i==2):
-        i=0
+    j=i
+    i=i+1
+    if (j==2):
         print 'Inside IF'
         tot=(t[0]+t[1]+t[2])/3
         print tot
-        tot=(tot*36*15*20/1000)
+        tot=((36*15*50)/(1000*tot))
         print tot
         url = "http://krsh.colorowebs.com/speedsense/update_traffic_data.php?location=2&speed=43"
         urllib2.urlopen(url).read()
@@ -32,5 +33,6 @@ while True:
         url = "http://krsh.colorowebs.com/speedsense/update_traffic_data.php?location=1&speed="+str(tot)
         urllib2.urlopen(url).read()
         print 'Push 2 Complete'
-    i=i+1
+        i=0
+
     ## GPIO.cleanup()
